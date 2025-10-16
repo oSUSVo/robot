8e600a8cf3698ce193a3720d9e245b2471ffcdcb
HaeSung <ho157157@naver.com>
2025-10-16 15:44:18 +0900

    4th commit

--- test.c
+++ test.c
@@ -1,1 +1,7 @@
-printf "hello world";
+#include <stdio.h>  // 표준 입출력 헤더파일 (printf, scanf 사용을 위해 필요)
+
+int main(void)  // 프로그램이 시작되는 함수 (main 함수)
+{
+    printf("Hello, World!\n");  // 화면에 문자열 출력
+    return 0;                   // 프로그램 정상 종료
+}
